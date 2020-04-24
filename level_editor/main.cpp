150db4d20202794a7fd44142173921735b287ae6
Ivan <ivan.asdf0@gmail.com>
2020-04-24 13:52:05 +0300

    Fixed windows building errors.

--- level_editor/main.cpp
+++ level_editor/main.cpp
@@ -2,6 +2,8 @@
 #include "core/globals.h"
 #include "level_editor.h"
 
+#undef main // https://stackoverflow.com/questions/6847360/error-lnk2019-unresolved-external-symbol-main-referenced-in-function-tmainc
+
 int main() {
     LevelEditor levelEditor;
     levelEditor.start();
